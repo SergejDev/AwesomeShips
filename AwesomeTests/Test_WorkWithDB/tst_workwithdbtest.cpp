29f73ca371f34a98cc268570449621bb1c458d30
Nadziejka <kvietacka@gmail.com>
2013-04-26 13:14:51 +0300

    Get_ functions remaked
    with defalt and other parameters

--- AwesomeTests/Test_WorkWithDB/tst_workwithdbtest.cpp
+++ AwesomeTests/Test_WorkWithDB/tst_workwithdbtest.cpp
@@ -17,15 +17,23 @@ private Q_SLOTS:
         void cleanupTestCase();
 
 
+        void GetCount_Default();
         void GetCount();
         void GetCount_data();
 
         void GetTopicID();
-        void GetLang();
+        void GetTopicID_Default();
+        void GetTopicID_data();
 
-//        void GetUserID();
+        void GetLang();
+        void GetLang_Default();
+        void GetLang_data();
+
         void GetLevel();
+        void GetLevel_data();
+
         void GetScore();
+        void GetScore_data();
 
         void CreateOpenDB();
         void CreateOpenDB_data();
@@ -45,43 +53,90 @@ void WorkWithDBTest::cleanupTestCase()
     qDebug()<<"End testing WorkWithDB class";
 }
 
+void WorkWithDBTest::GetCount_Default()
+{
+    WorkWithDB wdb("Words.s3db");
+    wdb.SetParams();
+    QCOMPARE(wdb.GetCount(), 10);
+}
+
 void WorkWithDBTest::GetCount()
 {
     WorkWithDB wdb("Words.s3db");
 
-//    QFETCH (int, count);
-//    QFETCH (int, res);
+    QFETCH (int, count);
+    QFETCH (int, res);
 
-    wdb.SetParams();
-    QCOMPARE(wdb.GetCount(), 10);
+    wdb.SetParams(count);
+    QCOMPARE(wdb.GetCount(), res);
+}
 
-    wdb.SetParams(5);
-    QCOMPARE(wdb.GetCount(), 5);
+void WorkWithDBTest::GetCount_data()
+{
+    QTest::addColumn<int> ("count");
+    QTest::addColumn<int> ("res");
+
+    QTest::newRow("count_test1") << 0 << 0;
+    QTest::newRow("count_test2") << 5 << 5;
+    QTest::newRow("count_test2") << 7 << 7;
 }
 
 void WorkWithDBTest::GetTopicID()
 {
     WorkWithDB wdb("Words.s3db");
+
+    QFETCH (int, topicID);
+    QFETCH (int, res);
+
+    wdb.SetParams(topicID);
+    QCOMPARE(wdb.GetTopicID(0, topicID), res);
+}
+
+void WorkWithDBTest::GetTopicID_Default()
+{
+    WorkWithDB wdb("Words.s3db");
     wdb.SetParams();
     QCOMPARE(wdb.GetTopicID(), 1);
+}
 
-    wdb.SetParams(0, 3);
-    QCOMPARE(wdb.GetTopicID(), 3);
+void WorkWithDBTest::GetTopicID_data()
+{
+    QTest::addColumn<int> ("topicID");
+    QTest::addColumn<int> ("res");
+
+    QTest::newRow("topicID_test1") << 10 << 10;
+    QTest::newRow("topicID_test2") << 15 << 15;
+    QTest::newRow("topicID_test2") << 7 << 7;
 }
 
 void WorkWithDBTest::GetLang()
 {
     WorkWithDB wdb("Words.s3db");
-    wdb.SetParams();
-    QCOMPARE(wdb.GetLang(), 0);
 
-    wdb.SetParams(1,1,5);
-    QCOMPARE(wdb.GetLang(), 5);
+    QFETCH(int, lang);
+    QFETCH(int, res);
+
+    wdb.SetParams(1,1, lang);
+    QCOMPARE(wdb.GetLang(), res);
 }
 
-//void WorkWithDBTest::GetUserID()
-//{
-//}
+void WorkWithDBTest::GetLang_Default()
+{
+    WorkWithDB wdb("Words.s3db");
+    wdb.SetParams();
+    QCOMPARE(wdb.GetLang(), 0);
+}
+
+void WorkWithDBTest::GetLang_data()
+{
+    QTest::addColumn<int> ("lang");
+    QTest::addColumn<int> ("res");
+
+    QTest::newRow("lang_test1") << 13 << 13;
+    QTest::newRow("lang_test2") << 20 << 20;
+    QTest::newRow("lang_test2") << 9 << 9;
+}
+
 
 void WorkWithDBTest::GetLevel()
 {
@@ -104,15 +159,7 @@ void WorkWithDBTest::CreateOpenDB()
     QCOMPARE(wdb.CreateOpenDB(dbName), result);
 }
 
-void WorkWithDBTest::GetCount_data()
-{
-    QTest::addColumn<int> ("count");
-    QTest::addColumn<int> ("res");
 
-    QTest::newRow("count_test1") << NULL << 10;
-    QTest::newRow("count_test2") << 5 << 5;
-
-}
 
 void WorkWithDBTest::CreateOpenDB_data()
 {
@@ -123,6 +170,8 @@ void WorkWithDBTest::CreateOpenDB_data()
     QTest::newRow("CreateOpenDB_test2") << "Users.s3db" << true;
     QTest::newRow("CreateOpenDB_test3") << "W.s3db" << false;
 }
+
+
 
 
 QTEST_APPLESS_MAIN(WorkWithDBTest)
